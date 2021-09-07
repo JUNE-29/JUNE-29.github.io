af10e7a78e9710513dc692fb541527fb9c41ef5c
JUNE-29 <jiyuni529@gmail.com>
2021-09-07 23:55:52 +0900

    [mod] login 기능 수정

--- js/login.js
+++ js/login.js
@@ -3,25 +3,28 @@ const loginInput = loginForm.querySelector("#username");
 const firstMassage = document.querySelector("#st-massage");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME="hidden";
+const USER_NAME="username";
+
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
-    firstMassage.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    firstMassage.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USER_NAME, username);
     printGreeting(username)
 }
 
 function printGreeting(username) {
     greeting.innerHTML = `Hello, ${username}`;
-    greeting.classList.remove("hideen");
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUserName = localStorage.getItem("username");
+const savedUserName = localStorage.getItem(USER_NAME);
 
 if(savedUserName === null) {
-    loginForm.classList.remove("hidden");
-    firstMassage.classList.remove("hidden");
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    firstMassage.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     printGreeting(savedUserName)
